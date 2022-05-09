93a7e7ededb28b6ae031fbcd168743c4bb8cfeb9
Christophe Langenberg <christophe@langenberg.be>
2022-05-09 10:00:11 +0200

    [CI] added github actions to test and push our docker image to ghcr

---
+++ app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import yaml
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route(
+    "/",
+    methods=["GET", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"],
+)
+def index():
+    return "Hello world\n"
+
+
+if __name__ == "__main__":
+    if os.path.isfile("config.yml"):
+        with open(os.path.join(os.getcwd(), "config.yml"), "r", encoding="utf-8") as f:
+            config = yaml.load(f, Loader=yaml.SafeLoader)
+
+        host = config["server"]["listen"]["address"]
+        port = config["server"]["listen"]["port"]
+    else:
+        host = "0.0.0.0"
+        port = 8080
+
+    app.run(debug=True, host=host, port=port)
